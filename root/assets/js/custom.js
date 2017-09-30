97558448573035a9dc9b48e355b48ba0d265cec2
oceanxy <xyzsyx@163.com>
2017-09-30 16:40:32 +0800

    submit

--- root/assets/js/custom.js
+++ root/assets/js/custom.js
@@ -1,11 +1,12 @@
 // Scroll Top
-$('.top').click(function() {
+$('.top').click(function () {
     $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 });
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() > $(window).height()) {
         $('.top').addClass("up");
     } else {
         $('.top').removeClass("up");
     }
 });
+
